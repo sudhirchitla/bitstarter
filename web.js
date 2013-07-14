f2ff3d652ad43ceb20dfc9579d45f4704c1b4b54
Sudhir Chitla <sudhirchitla@gmail.com>
2013-07-14 13:30:55 +0000

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
+var fs = require('fs');
+fs.readFile('index.html', function (err, data) {
+        if (err) throw err;
+        var buffer = data;
+        var data1 = buffer.toString("utf-8");
+	};
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data1);
 });
 
 var port = process.env.PORT || 5000;
