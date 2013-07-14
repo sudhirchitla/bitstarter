f8700ae7968582d7145502876b5ef654e13a34bd
Sudhir Chitla <sudhirchitla@gmail.com>
2013-07-14 14:48:48 +0000

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response) {
 
 var fs = require('fs');
 var data = fs.readFileSync('index.html');
-response.send(data.toString("utf-8", 0, 26);
+response.send(data.toString("utf-8", 0, 26));
 });
 
 var port = process.env.PORT || 5000;
