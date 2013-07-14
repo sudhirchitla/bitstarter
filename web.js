e1905dafd6a213372afb57ab1841a30376deff92
Sudhir Chitla <sudhirchitla@gmail.com>
2013-07-14 13:43:12 +0000

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -12,7 +12,15 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send(data1);
-   response.send('welcome sudhir');
+
+var fs = require('fs');
+fs.readFile('index.html', function (err, data) {
+        if (err) throw err;
+        var buffer = data;
+        var data1 = buffer.toString("utf-8");
+        };
+
+   response.send(data1);
 });
 
 var port = process.env.PORT || 5000;
