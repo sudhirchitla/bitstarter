91c1ff3b93e48297ed3723feb7b750e839e2bfc6
Sudhir Chitla <sudhirchitla@gmail.com>
2013-07-14 13:34:17 +0000

    Updated web.js to read index.html less

--- web.js
+++ web.js
@@ -1,16 +1,18 @@
+/*
 var fs = require('fs');
 fs.readFile('index.html', function (err, data) {
         if (err) throw err;
         var buffer = data;
         var data1 = buffer.toString("utf-8");
 	};
-
+*/
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(data1);
+//  response.send(data1);
+   response.send('welcome sudhir');
 });
 
 var port = process.env.PORT || 5000;
